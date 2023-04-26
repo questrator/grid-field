903b127629ad9b7fa0ec1a78a47dbff3f44a00c1
Slava Ivanov <drumtrack@ya.ru>
2023-04-26 18:06:05 +0300

    upd

---
+++ js/grid.js
@@ -0,0 +1,30 @@
+const grid = document.querySelector("section.grid");
+let mouseDown = false;
+grid.addEventListener("mousemove", cellClickHandler);
+grid.addEventListener("mousedown", checkMouseDown)
+grid.addEventListener("mouseup", checkMouseUp)
+
+for (let i = 1; i <= 2501; i++) {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    cell.setAttribute("data-alive", "0");
+    cell.setAttribute("data-order", i % 2 ? "odd" : "even");
+    grid.insertAdjacentElement("beforeend", cell);
+}
+
+function cellClickHandler(event) {
+    if (mouseDown) {
+        if (event.target.dataset.alive == 0) event.target.dataset.alive = 1;
+        else event.target.dataset.alive = 0;
+    }    
+}
+
+function checkMouseDown(event) {
+    mouseDown = true;
+    console.log(mouseDown)
+}
+
+function checkMouseUp(event) {
+    mouseDown = false;
+    console.log(mouseDown)
+}
